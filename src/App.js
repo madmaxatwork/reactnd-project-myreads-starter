cc9c19720c7ae317b7a90b96432cb6002b4d7ee6
madmaxatwork <tejasjukar@gmail.com>
2019-04-14 19:57:09 -0700

    Added Browser Router + Added api to get all books

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import * as BooksAPI from './BooksAPI';
 import Shelf from './Shelf'
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom'
+import List from './List'
+
 
 class BooksApp extends React.Component {
   state = {
@@ -13,49 +16,28 @@ class BooksApp extends React.Component {
      */
     showSearchPage: true,
     books: []
-  };
+  }
+
+  componentDidMount() {
+    this.retrieveAllBooks();
+  }
+
+  // This will get all the required books. Can be extended 
+  retrieveAllBooks() {
+    BooksAPI.getAll()
+      .then(books => this.setState({ books }))
+      .catch(() => { alert('Something messed up :().'); });
+  }
 
   render() {
     return (
-      <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <Shelf title="Currently Reading" />
-                <Shelf title="Want to Read" />
-                <Shelf title="Read" />
-              </div>
-            </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
-        )}
+      <BrowserRouter>
+      <div className='app'>
+        <Route exact path='/' render={() => (
+          <List />
+        )} />
       </div>
+    </BrowserRouter>
     )
   }
 }
